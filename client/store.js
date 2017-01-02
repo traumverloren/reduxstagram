a81bccbef1c1d0d9c99d7c226ac338b2d384fdfd
Stephanie Nemeth <traumverloren@gmail.com>
2017-01-02 14:03:50 +0100

    shows photo components now

--- client/store.js
+++ client/store.js
@@ -1,11 +1,10 @@
-import { createStore, compose } from 'redux';
+import { createStore, compse } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
 // import the root reducer
 import rootReducer from './reducers/index';
 
-// import dummy data
 import comments from './data/comments';
 import posts from './data/posts';
 
