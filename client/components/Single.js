1cd8f2714c136437e05c36ce7f49213475d4a2a5
Stephanie Nemeth <traumverloren@gmail.com>
2017-01-02 14:30:40 +0100

    add comments component, update single view to show likes and comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments'
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    // get us the post
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-      I'm the single
-
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     )
   }
